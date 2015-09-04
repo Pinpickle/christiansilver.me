9fd1a251d6a3b506169e3073ca436ccf825d0cea
Christian Silver <christian@decode.org.nz>
2015-09-04 16:20:09 +1200

    Fit projects listing into a full-size grid

--- src/assets/scripts/contexts/page-projects.js
+++ src/assets/scripts/contexts/page-projects.js
@@ -2,6 +2,20 @@
 var $ = require('jquery');
 var Silver = require('../silver.js');
 
+function onResize(e) {
+  var width = $(window).width();
+  var maxWidth = 300;
+  if (width < 600) {
+    maxWidth = 200;
+  }
+
+  var boxNumber = Math.max(1, Math.floor(width / maxWidth));
+  var percentage = (100 / boxNumber) + '%';
+
+  $('.projects-listing-element').css({ width: percentage });
+}
+
 Silver.contexts.page_projects = function () {
-  
+  onResize();
+  $(window).resize(onResize)
 };
