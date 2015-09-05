c379f3f679a76dc602dc948da8e5e5e1be9e6a9c
Christian Silver <christian@decode.org.nz>
2015-09-06 00:10:16 +1200

    Add sticky header for image headers

---
+++ src/assets/scripts/contexts/layout-default.js
@@ -0,0 +1,29 @@
+var $ = require('jquery');
+var Silver = require('../silver.js');
+
+Silver.contexts.layoutDefault = function () {
+  var $header = $('.site-header');
+  var $headerContent = $('.site-header-content');
+  var headerTop = 0;
+  var headerSnapped = false;
+
+  if ($())
+
+  $(window).on('silver:resize', function () {
+    headerTop = $headerContent.offset().top;
+  });
+
+  if ($header.hasClass('site-header-image')) {
+    $(window).on('silver:scroll', function () {
+      if ((headerSnapped) && (Silver.scroll < headerTop)) {
+        $header.removeClass('site-header-snapped');
+        headerSnapped = false;
+      } else if ((!headerSnapped) && (Silver.scroll >= headerTop)) {
+        $header.addClass('site-header-snapped');
+        headerSnapped = true;
+      }
+    });
+  }
+
+  // Any special home page logic goes here!
+};
--- src/assets/scripts/silver.js
+++ src/assets/scripts/silver.js
@@ -3,11 +3,56 @@ var $ = require('jquery');
 var bulk = require('bulk-require');
 
 var Silver = {
-  contexts: { }
+  contexts: { },
+  width: 0,
+  height: 0,
+  scroll: 0
 };
 
-Silver.contexts.common = function common() {
+function throttle(fn, threshhold, scope) {
+  threshhold = threshhold || (threshhold = 250);
+  var last;
+  var deferTimer;
+  return function () {
+    var context = scope || this;
 
+    var now = +(new Date());
+    var args = arguments;
+    if (last && now < last + threshhold) {
+      // hold on to it
+      clearTimeout(deferTimer);
+      deferTimer = setTimeout(function () {
+        last = now;
+        fn.apply(context, args);
+      }, threshhold);
+    } else {
+      last = now;
+      fn.apply(context, args);
+    }
+  };
+}
+
+var $window = $(window);
+var resize = function () {
+  Silver.width = $window.width();
+  Silver.height = $window.height();
+
+  $window.trigger('silver:resize');
+};
+
+var scroll = function () {
+  Silver.scroll = $window.scrollTop();
+  $window.trigger('silver:scroll');
+}
+
+Silver.contexts.common = function common() {
+  $window.resize(throttle(resize, 200));
+  $window.scroll(throttle(scroll, 30));
+};
+
+Silver.contexts.post = function post() {
+  resize();
+  scroll();
 };
 
 Silver.run = function () {
@@ -20,6 +65,8 @@ Silver.run = function () {
         main.contexts[item]();
       }
     });
+
+    main.contexts.post();
   };
 
   $(document).ready(main.ready);
